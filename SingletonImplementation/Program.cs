fd9816164372c0a2c80ad0362e524c93b828d16b
Reece Wanvig <rwanvig@varsitycollege.co.za>
2023-03-07 11:40:39 +0200

    Add project files.

---
+++ SingletonImplementation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SingletonImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lunch Myorder = Lunch.getInstance();
+            Myorder.setLunchOrder("Ham and cheese on bread");
+            Lunch Myorder2 = Lunch.getInstance();
+            Myorder2.setLunchOrder("Mayo on bread");
+            Lunch Myorder3 = Lunch.getInstance();   
+            Console.WriteLine(Myorder3.getLunchOrder());
+        }
+    }
+}
---
+++ prjSingletonRules/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using prjSingletonRules;
+//Util S = new DbUtil();
+DbUtil one = DbUtil.getInstance();
+DbUtil two = DbUtil.getInstance();
+DbUtil three = DbUtil.getInstance();
